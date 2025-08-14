53fa98f231c7efa026410d198d259b67feb5f068
andoricano <andoricano@gmail.com>
2025-08-15 00:42:13 +0900

    [feat] change button layout to new in HomeScreen.

--- app/src/main/java/com/andro/analogclocktimer/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/andro/analogclocktimer/ui/screen/HomeScreen.kt
@@ -1,16 +1,20 @@
 package com.andro.analogclocktimer.ui.screen
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,12 +24,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.andro.analogclocktimer.App.Companion.app
 import com.andro.analogclocktimer.MainViewModel
 import com.andro.analogclocktimer.navigateToTheme
 import com.andro.analogclocktimer.ui.canvas.clock.ClockCanvas
+import com.andro.analogclocktimer.ui.composable.BasicButton
 import kotlinx.coroutines.flow.update
 
 @SuppressLint("UnusedBoxWithConstraintsScope")
@@ -47,7 +53,7 @@ fun HomeScreen(nc: NavController,vm : MainViewModel = hiltViewModel()) {
         verticalArrangement = Arrangement.Center
     ) {
         BoxWithConstraints {
-            val side = minOf(maxWidth, maxHeight) // scope 변수 사용
+            val side = minOf(maxWidth, maxHeight)
 
             BoxWithConstraints(
                 modifier = Modifier.size(side)
@@ -58,12 +64,30 @@ fun HomeScreen(nc: NavController,vm : MainViewModel = hiltViewModel()) {
             }
         }
         if (menuVisible) {
-            Column {
-                Button(onClick = { nc.navigateToTheme() }) {
-                    Text("Go to Theme")
+            Column (modifier = Modifier.padding(20.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)){
+                BasicButton("Go to Theme",
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.ArrowForward,
+                            contentDescription = "Arrow",
+                            tint = Color.White
+                        )
+                    }) {
+                    nc.navigateToTheme()
                 }
-                Button(onClick = { vm.menuState.update { false } }) {
-                    Text("Hidden Buttons")
+
+                BasicButton(
+                    "Hide Buttons",
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowDown,
+                            contentDescription = "Arrow",
+                            tint = Color.White
+                        )
+                    }
+                ) {
+                    vm.menuState.update { false }
                 }
             }
         }
